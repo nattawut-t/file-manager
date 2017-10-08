365ff3c2cfa1ea4280eb31abf0dcdc3430808a31
j2sdk_ball2 <codecrazii@gmail.com>
2017-10-08 19:12:19 +0700

    add modal dialog

--- src/presentations/Layout.js
+++ src/presentations/Layout.js
@@ -8,22 +8,16 @@ import {
 } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import Home from './Home'
-import FileInput from './FileInput'
-import Test from './Test'
+import File from './File'
+import Modal from './Modal'
 
-class Layout extends Component {
-
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/file" component={FileInput} />
-          <Route path="/test" component={Test} />
-        </Switch>
-      </div>
-    )
-  }
-}
+const Layout = () =>
+  <div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/file" component={File} />
+      <Route path="/modal" component={Modal} />
+    </Switch>
+  </div>
 
 export default withRouter(Layout)
